2e9e5b51f4b2c1bdc73f406b87e007f89fdb11f8
Thufir <hawat.thufir@gmail.com>
2015-03-11 03:29:13 -0700

    init

---
+++ SimpleGreeterEAR-ejb/src/java/net/bounceme/doge/ejb/ContactsSessionBean.java
@@ -0,0 +1,26 @@
+package net.bounceme.doge.ejb;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateless
+public class ContactsSessionBean implements ContactsSessionBeanRemote {
+
+    @EJB
+    private ContactsFacadeLocal contactsFacade;
+
+    @Override
+    public String getAllContacts() {
+      //  List<Contacts> all = contactsFacade.findAll();
+        List<String> all = new ArrayList<>();
+        all.add("fred");
+        all.add("flinstone");
+        all.add("barney");
+        all.add("rubble");
+        all.add("wilma");
+        return all.toString();
+    }
+
+}
